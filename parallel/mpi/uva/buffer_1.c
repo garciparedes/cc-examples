9c45564dc584a61d3e45929cb3eaeba836ca6913
Sergio Garcia <sergio@garciparedes.me>
2017-03-22 11:20:35 +0100

    Changes on example

--- parallel/mpi/uva/buffer_1.c
+++ parallel/mpi/uva/buffer_1.c
@@ -14,19 +14,18 @@ int main(int argc, char *argv[]) {
     int tag = 0;
 
     int i;
-    int *data = (int *)malloc( MENSSAGE_COUNT * sizeof(int) );
+    char *data = (char *)malloc( MENSSAGE_COUNT * sizeof(char) );
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (world_rank == RANK_SENDER)
-    {
+    if (world_rank == RANK_SENDER) {
     	int buffer_size = MENSSAGE_COUNT*(sizeof(int) + MPI_BSEND_OVERHEAD);
     	int *buffer = (int *) malloc(buffer_size);
 
         for (i = 0; i < MENSSAGE_COUNT; i++) {
-            data[i] = (MENSSAGE_COUNT - 1)  - i;
+            data[i] = 'a'  + i;
         }
 
         printf("Sending...\n");
@@ -48,7 +47,7 @@ int main(int argc, char *argv[]) {
         printf("Received!\n");
 
         for (i = 0; i < MENSSAGE_COUNT; i++) {
-            printf("%d  ", data[i]);
+            printf("%c  ", data[i]);
         }
         printf("\n");
 
